54ebbc889fa0cf9fbff22363bb5a8dd3234c427b
amit.kumar <amit.kumar@otssolutions.com>
2023-06-12 17:36:00 +0530

    login and other pages basic validation added

--- public/js/commonJs/main.js
+++ public/js/commonJs/main.js
@@ -1,3 +1,9 @@
 const changePageHeading = (pageHeading) => {
     document.title = pageHeading
 }
+
+
+const getRequest = async () => {
+    const axiosClient = await axios.get('https://jsonplaceholder.typicode.com/todos')
+    console.log(axiosClient);
+}
