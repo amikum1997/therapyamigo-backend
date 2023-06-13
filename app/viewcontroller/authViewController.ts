d8cdab1ef3bcf71f50662632d45143341d00e5a4
amit.kumar <amit.kumar@otssolutions.com>
2023-06-13 14:34:43 +0530

    dashboard redirect completed

--- app/viewcontroller/authViewController.ts
+++ app/viewcontroller/authViewController.ts
@@ -8,32 +8,7 @@ const authViewController = {
             if (!req.user) {
                 return res.render("authentication/login")
             } else {
-                const userDetail: any = req.user;
-                if (userDetail.userRole === "USER") {
-                    return res.render("dashboard/userDashboard/clientDashboard", {
-                        user: JSON.stringify(userDetail)
-                    })
-                }
-                if (userDetail.userRole === "ADMIN") {
-                    return res.render("dashboard/userDashboard/adminDashboard", {
-                        user: userDetail
-                    })
-                }
-                if (userDetail.userRole === "CORPORATE") {
-                    return res.render("dashboard/userDashboard/corporateDashboard", {
-                        user: userDetail
-                    })
-                }
-                if (userDetail.userRole === "COUNSELOR") {
-                    return res.render("dashboard/userDashboard/counselorDashboard", {
-                        user: userDetail
-                    })
-                }
-                if (userDetail.userRole === "VENDOR") {
-                    return res.render("dashboard/userDashboard/vendorDashboard", {
-                        user: userDetail
-                    })
-                }
+                return res.redirect('/dashboard')
             }
         } catch (err) {
             next(err)
