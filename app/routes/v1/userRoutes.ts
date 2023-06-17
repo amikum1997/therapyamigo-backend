addca9fce2c7bff1b61cf21917a4b56165c2922b
Programming Maze <ams281997@gmail.com>
2023-06-18 01:54:30 +0530

    update

--- app/routes/v1/userRoutes.ts
+++ app/routes/v1/userRoutes.ts
@@ -7,11 +7,26 @@ const userRouter = express.Router()
 userRouter.route('/dashboard').get(userViewController.userDashbaord)
 userRouter.route('/seo-dashboard').get(userViewController.websiteAnalyticsDashboard)
 userRouter.route('/helpdesk-dashboard').get(userViewController.helpDeskAnalyticsDashboard)
+// ADMIN MANAGE SCREENS
 userRouter.route('/manage/counselors').get(userViewController.manageCounselors)
 userRouter.route('/manage/all-clients').get(userViewController.websiteUsers)
 userRouter.route('/manage/transactions').get(userViewController.manageTransaction)
 userRouter.route('/manage/internal-tickets').get(userViewController.internalTicketManagement)
 userRouter.route('/manage/corporate-management').get(userViewController.corporateManagement)
 userRouter.route('/manage/vendor-management').get(userViewController.vendorManagement)
+// ADMIN REPORT SCREENS
+userRouter.route('/reporting/counselor-performance').get(userViewController.counselorPerformance)
+userRouter.route('/reporting/counselor-pay').get(userViewController.counselorPay)
+userRouter.route('/reporting/client-report').get(userViewController.clientReport)
+userRouter.route('/reporting/corporate-report').get(userViewController.corporateReport)
+userRouter.route('/reporting/vendor-report').get(userViewController.vendorReport)
+userRouter.route('/reporting/assessment-report').get(userViewController.assessmentReport)
+// ADMIN MANAGE CONTENT
+userRouter.route('/manage-content/admin-blog-listing').get(userViewController.adminBlogListing)
+userRouter.route('/manage-content/blog').get(userViewController.newBlog)
+userRouter.route('/manage-content/admin-video-lising').get(userViewController.adminVideoListing)
+userRouter.route('/manage-content/admin-assessment-lising').get(userViewController.assessmentListing)
+userRouter.route('/manage-content/assessment').get(userViewController.newAssessment)
+
 
 export default userRouter;
--- app/viewcontroller/userViewController.ts
+++ app/viewcontroller/userViewController.ts
@@ -125,6 +125,127 @@ const userViewController = {
                 user: JSON.stringify(userDetail)
             })
         }
+    },
+    adminBlogListing: async (req: IRequest, res: IResponse, next: INext) => {
+        const userDetail: any = req.user;
+        if (!userDetail || userDetail.userRole != "ADMIN") {
+            res.render("screens/utility/error404" , {layout: "layouts/clientLayout"})
+        }else{
+            return res.render("screens/dashboard/adminBlogListing", {
+                layout: "layouts/dashboardLayout",
+                user: JSON.stringify(userDetail)
+            })
+        }
+    },
+    adminVideoListing: async (req: IRequest, res: IResponse, next: INext) => {
+        const userDetail: any = req.user;
+        if (!userDetail || userDetail.userRole != "ADMIN") {
+            res.render("screens/utility/error404" , {layout: "layouts/clientLayout"})
+        }else{
+            return res.render("screens/dashboard/adminVideoListing", {
+                layout: "layouts/dashboardLayout",
+                user: JSON.stringify(userDetail)
+            })
+        }
+    },
+    assessmentListing: async (req: IRequest, res: IResponse, next: INext) => {
+        const userDetail: any = req.user;
+        if (!userDetail || userDetail.userRole != "ADMIN") {
+            res.render("screens/utility/error404" , {layout: "layouts/clientLayout"})
+        }else{
+            return res.render("screens/dashboard/assessmentListing", {
+                layout: "layouts/dashboardLayout",
+                user: JSON.stringify(userDetail)
+            })
+        }
+    },
+    assessmentReport: async (req: IRequest, res: IResponse, next: INext) => {
+        const userDetail: any = req.user;
+        if (!userDetail || userDetail.userRole != "ADMIN") {
+            res.render("screens/utility/error404" , {layout: "layouts/clientLayout"})
+        }else{
+            return res.render("screens/dashboard/assessmentReport", {
+                layout: "layouts/dashboardLayout",
+                user: JSON.stringify(userDetail)
+            })
+        }
+    },
+    clientReport: async (req: IRequest, res: IResponse, next: INext) => {
+        const userDetail: any = req.user;
+        if (!userDetail || userDetail.userRole != "ADMIN") {
+            res.render("screens/utility/error404" , {layout: "layouts/clientLayout"})
+        }else{
+            return res.render("screens/dashboard/clientReport", {
+                layout: "layouts/dashboardLayout",
+                user: JSON.stringify(userDetail)
+            })
+        }
+    },
+    corporateReport: async (req: IRequest, res: IResponse, next: INext) => {
+        const userDetail: any = req.user;
+        if (!userDetail || userDetail.userRole != "ADMIN") {
+            res.render("screens/utility/error404" , {layout: "layouts/clientLayout"})
+        }else{
+            return res.render("screens/dashboard/corporateReport", {
+                layout: "layouts/dashboardLayout",
+                user: JSON.stringify(userDetail)
+            })
+        }
+    },
+    counselorPay: async (req: IRequest, res: IResponse, next: INext) => {
+        const userDetail: any = req.user;
+        if (!userDetail || userDetail.userRole != "ADMIN") {
+            res.render("screens/utility/error404" , {layout: "layouts/clientLayout"})
+        }else{
+            return res.render("screens/dashboard/counselorPay", {
+                layout: "layouts/dashboardLayout",
+                user: JSON.stringify(userDetail)
+            })
+        }
+    },
+    counselorPerformance: async (req: IRequest, res: IResponse, next: INext) => {
+        const userDetail: any = req.user;
+        if (!userDetail || userDetail.userRole != "ADMIN") {
+            res.render("screens/utility/error404" , {layout: "layouts/clientLayout"})
+        }else{
+            return res.render("screens/dashboard/counselorPerformance", {
+                layout: "layouts/dashboardLayout",
+                user: JSON.stringify(userDetail)
+            })
+        }
+    },
+    newAssessment: async (req: IRequest, res: IResponse, next: INext) => {
+        const userDetail: any = req.user;
+        if (!userDetail || userDetail.userRole != "ADMIN") {
+            res.render("screens/utility/error404" , {layout: "layouts/clientLayout"})
+        }else{
+            return res.render("screens/dashboard/newAssessment", {
+                layout: "layouts/dashboardLayout",
+                user: JSON.stringify(userDetail)
+            })
+        }
+    },
+    newBlog: async (req: IRequest, res: IResponse, next: INext) => {
+        const userDetail: any = req.user;
+        if (!userDetail || userDetail.userRole != "ADMIN") {
+            res.render("screens/utility/error404" , {layout: "layouts/clientLayout"})
+        }else{
+            return res.render("screens/dashboard/newBlog", {
+                layout: "layouts/dashboardLayout",
+                user: JSON.stringify(userDetail)
+            })
+        }
+    },
+    vendorReport: async (req: IRequest, res: IResponse, next: INext) => {
+        const userDetail: any = req.user;
+        if (!userDetail || userDetail.userRole != "ADMIN") {
+            res.render("screens/utility/error404" , {layout: "layouts/clientLayout"})
+        }else{
+            return res.render("screens/dashboard/vendorReport", {
+                layout: "layouts/dashboardLayout",
+                user: JSON.stringify(userDetail)
+            })
+        }
     }
 
 }
